e708e51b945c9c6601276be9d6b7d399136d6d44
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2024-03-26 10:41:10 -0500

    Cambios

--- src/config/AxiosClient.ts
+++ src/config/AxiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosClient: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL_DEV,
+  baseURL: import.meta.env.VITE_API_URL_PRODUCTION,
 });
 
 export default axiosClient;
